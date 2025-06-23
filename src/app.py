ee25de452f4c2903fb46fff430b1b25e4da3900c
kevit-shubham-kushwaha <shubham.kushwaha@kevit.io>
2025-06-23 08:44:20 +0530

    feat(log): added middleware to keep track of requests path in log

--- src/app.py
+++ src/app.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
 
+from libs.utils.config import config
+from src import logger
+
 app = FastAPI()
 
+logger.info("Starting FastAPI application")
 
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
-
 
 @app.get("/")
 def read_root():
@@ -15,4 +18,7 @@ def read_root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    if config.HOST is None or config.PORT is None:
+        raise ValueError("HOST and PORT must be set in the configuration.")
+    
+    uvicorn.run(app, host=config.HOST, port=int(config.PORT))
